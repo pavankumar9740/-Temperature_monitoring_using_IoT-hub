60290c9080b166b625dc1fccf67eb8cde8df0663
pavan <pavanvkumar9@gmail.com>
2020-12-01 16:30:03 +0530

    modify

--- Dht11Azure.py
+++ Dht11Azure.py
@@ -44,22 +44,6 @@ if __name__ == '__main__':
     print("Press Ctrl-C to exit")
     iothub_client_telemetry_sample_run()
 
-   ''' import RPi.GPIO as GPIO
-    import Adafruit_DHT
-    import time
-
-    DHT_SENSOR = Adafruit_DHT.DHT11
-    DHT_PIN = 4
-
-    while True:
-        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-        if humidity is not None and temperature is not None:
-
-            print("Temp={0:0.1f}C Humidity={1:0.1F}%".format(temperature, humidity))
-        else:
-            print("Sensor failure")
-        time.sleep(1)'''
-
     import RPi.GPIO as GPIO
     import dht11
     import time
